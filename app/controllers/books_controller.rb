6a300af5f3f3d0b4c6b7875c952b2a83a0b8e916
EC2 Default User <ec2-user@ip-172-31-22-226.ec2.internal>
2021-06-21 13:58:17 +0000

    submit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_current_user, only: [:edit, :update, :destroy]
+
+
+
+  def create
+    @user = current_user
+    @book= Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render "index"
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book= Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new= Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+   params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_current_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b1103cc6f63f00d6f8ee9e5f0b6f10d6fbb02da45435cc431386a553e98b9c572bfdfc4cd003b82e7363d59b7b20dfff4c5fe51fcaded5e0ed131f1a9723c328'
